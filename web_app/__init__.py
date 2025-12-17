75f81f38fdf95f01e9234d55ce2d8d89b10c474f
smh389-gtown <smh389@georgetown.edu>
2025-12-17 11:36:54 -0500

    added in logic and html for guessing game

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,24 +1,25 @@
-# this is the "web_app/__init__.py" file...
-
 import os
 from dotenv import load_dotenv
 from flask import Flask
 
+from web_app.routes.game_routes import game_bp
 from web_app.routes.home_routes import home_routes
-
-
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") # set this to something else on production!!!
-
+SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-change-me")
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = SECRET_KEY
+
+    # register blueprints
     app.register_blueprint(home_routes)
+    app.register_blueprint(game_bp, url_prefix="/game")
+
+
     return app
 
 if __name__ == "__main__":
-    my_app = create_app()
-    my_app.run(debug=True)
+    app = create_app()
+    app.run(debug=True)
